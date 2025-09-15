ee5b50594fe4cf41ef41c272a7fd83bfd601aac8
Binod Sharma <thebinod404@gmail.com>
2025-09-15 12:20:31 +0545

    Implement TailwindCSS

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-500">
+      <Text className=" text-white text-xl">Welcome to my App !</Text>
+    </View>
+  );
+}
